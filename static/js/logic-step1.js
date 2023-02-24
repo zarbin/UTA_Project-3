a3c1d37eb8410d9f333d304b81b5cac3341a498a
quinn-johndavid <quinn.johndavid@gmail.com>
2023-02-23 20:50:23 -0600

    added in more map code

--- static/js/logic-step1.js
+++ static/js/logic-step1.js
@@ -1,40 +1,100 @@
+
+
 // Creating the map object
+var url = '/data_map'
+
+//var geojson; 
+
+//Get the data with d3.
+
+    // Adding the tile layer
+var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var satalite = L.tileLayer('https://core-sat.maps.yandex.net/tiles?l=sat&x={x}&y={y}&z={z}&scale=1&lang=ru_RU', {
+        attribution: '<a href="https://yandex.ru" target="_blank">Yandex</a>'
+});
+
+var baseMaps = {
+        "<span style='color: blue'>Street Map</span>": base,
+        "<span style='color: blue'>Satalite Map</span>": satalite
+};    
+
 var myMap = L.map("map", {
-  center: [30.3, -97.65],
-  zoom: 11
-});
+        center: [38.3, -97.36],
+        zoom: 4.5,
+        layers: [base]
+});  
 
-// Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+//base.addTo(myMap);
+L.control.layers(null, baseMaps,).addTo(myMap);  
 
-// Load the GeoJSON data.
-var geoData = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/ACS-ED_2014-2018_Economic_Characteristics_FL.geojson";
+    // Set up the legend.
+var legend = L.control({position: 'bottomright'});
 
-var geojson;
 
-// Get the data with d3.
-d3.json(geoData).then(function(data) {
+legend.onAdd = function(){
 
-  console.log(data);
+        // Create the legend div
+    var div = L.DomUtil.create('div', 'info legend'),
+            levels = [1, 10, 30, 50, 100, 200, 500],
+            labels = [];
 
-  // Create a new choropleth layer.
+        // create legend title
+    div.innerHTML += '<center><h2>Job<br>Postings</h2><hr></center>'
 
-    // Define which property in the features to use.
+    for (var i = 0; i < levels.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(levels[i] + 1) + '"></i> '  +
+            levels[i] + (levels[i + 1] ? '&ndash;' + levels[i + 1] + ' <br>' : ' +');
+        };
+    return div;
+};
 
-    // Set the color scale.
+legend.addTo(myMap);
+    
 
-    // The number of breaks in the step range
+function getColor(depth){
+    
+    // Deeper depths have darker colors
+    if (depth > 500){
+        return '#b10026'
+    } else if (depth > 200){
+        return '#e31a1c';
+    } else if (depth > 100){
+        return '#fc4e2a'
+    } else if(depth > 50) {
+        return '#fd8d3c'
+    } else if (depth > 30) {
+        return '#feb24c'
+    } else if (depth > 10) {
+        return '#fed976'
+    } else {
+        return '#ffffb2'
+    };
+};
 
-    // q for quartile, e for equidistant, k for k-means
+d3.json(url).then(function(response, ) {
+    console.log(response);
+    
+    function ptToLayer(latlng) {
+        return L.circleMarker(latlng, {
+            color: 'green',
+            weight: 1,
+            //fillColor: getColor(response.latitude, response.longitude),
+            fillOpacity: 0.6,
+            //radius: feature.properties.mag ** 1.5,
+        });
+    };
 
-    // Binding a popup to each layer
+    var gJsonLayer = L.geoJson(response, {
+        //onEachFeature: onEach,
+        pointToLayer: ptToLayer
+    });
 
-  // Set up the legend.
+    myMap.addLayer(gJsonLayer);
+    
+}); 
 
-    // Add the minimum and maximum
 
-  // Adding the legend to the map.
-
-});
